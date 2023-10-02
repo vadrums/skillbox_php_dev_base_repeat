7c91f7a5d618734af681ef3f38d442d6fd7c1af7
vadrums <vadlambrianov@gmail.com>
2023-10-02 17:15:18 +0300

    first script added

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Перезагрузка курса по PHP</title>
-</head>
-<body>
-<h1 class="start-page__title">Перезагрузка курса по PHP</h1>
-<div class="start-page__container">
-    <div class="section section_white">
-        <div class="top_">
-        </div>
-    </div>
-</div>
-</body>
-</html>
+<?php
+
+echo date('r');
+
